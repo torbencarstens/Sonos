052e996c0b8cc06cef8ad7add07e99f6587cb8d6
chabare <chabare95@gmail.com>
2017-08-30 16:49:31 +0200

    Use stderr for logs > info

--- src/main.rs
+++ src/main.rs
@@ -21,13 +21,32 @@ fn setup_logging(level: log::LogLevelFilter, file: Option<std::path::PathBuf>) -
         ))
     }).level(level);
 
-    let dispatcher = match file {
-        Some(path) => {
-            dispatcher.chain(fern::log_file(path)?)
+
+    let stderr_dispatcher = fern::Dispatch::new().filter(|data| {
+        match data.level().cmp(&log::LogLevelFilter::Warn.to_log_level().unwrap()) {
+            std::cmp::Ordering::Less => true,
+            std::cmp::Ordering::Equal => true,
+            _ => false
         }
-        None => dispatcher.chain(std::io::stdout())
+    }).chain(std::io::stderr());
+
+    let stdout_dispatcher = fern::Dispatch::new().filter(|data| {
+        match data.level().cmp(&log::LogLevelFilter::Warn.to_log_level().unwrap()) {
+            std::cmp::Ordering::Less => false,
+            std::cmp::Ordering::Equal => false,
+            _ => true
+        }
+    });
+
+
+    let stdout_dispatcher = match file {
+        Some(path) => {
+            stdout_dispatcher.chain(fern::log_file(path)?)
+        }
+        None => stdout_dispatcher.chain(std::io::stdout())
     };
 
+    let dispatcher = dispatcher.chain(stderr_dispatcher).chain(stdout_dispatcher);
     dispatcher.apply()?;
 
     Ok(())
