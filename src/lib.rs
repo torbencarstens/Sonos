fff58be57b91f190f9c801fb4dc93c2f8b2751b6
chabare <chabare95@gmail.com>
2017-09-04 22:13:10 +0200

    Add use for super in lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,10 @@ mod errors;
 
 use errors::SonosError;
 use sonos_discovery::Discover;
+use std::{collections, net};
 
 
-pub fn discover(timeout: Option<u32>, device_count: Option<usize>) -> Result<std::collections::HashSet<std::net::IpAddr>, SonosError> {
+pub fn discover(timeout: Option<u32>, device_count: Option<usize>) -> Result<collections::HashSet<net::IpAddr>, SonosError> {
     match Discover::new()?.start(timeout, device_count) {
         Ok(set) => Ok(set),
         Err(e) => Err(SonosError::from(e))
