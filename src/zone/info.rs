afe5db2eeadacda60c06f815bf09fbcc580b308f
chabare <chabare95@gmail.com>
2017-09-09 20:06:22 +0200

    Add ZoneInfo

---
+++ src/zone/info.rs
@@ -0,0 +1,21 @@
+use errors::SonosError;
+use super::futures;
+
+#[derive(Debug)]
+pub struct ZoneInfo {}
+
+impl ZoneInfo {
+    pub fn new() -> Self {
+        debug!("Creating new ZoneInfo");
+        ZoneInfo {}
+    }
+}
+
+impl futures::Future for ZoneInfo {
+    type Item = ZoneInfo;
+    type Error = SonosError;
+
+    fn poll(&mut self) -> futures::Poll<Self::Item, Self::Error> {
+        unimplemented!()
+    }
+}
