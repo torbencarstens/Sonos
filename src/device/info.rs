8e4ba3dc1bdacc5897cad80b7a86f683f079f868
chabare <chabare95@gmail.com>
2017-09-09 20:05:03 +0200

    Use DeviceInfo as Future

--- src/device/info.rs
+++ src/device/info.rs
@@ -1,3 +1,21 @@
-pub struct DeviceInfo {
+use errors::SonosError;
+use super::futures;
 
+#[derive(Debug)]
+pub struct DeviceInfo {}
+
+impl DeviceInfo {
+    pub fn new() -> Self {
+        debug!("Creating new DeviceInfo");
+        DeviceInfo {}
+    }
+}
+
+impl futures::Future for DeviceInfo {
+    type Item = DeviceInfo;
+    type Error = SonosError;
+
+    fn poll(&mut self) -> futures::Poll<Self::Item, Self::Error> {
+        unimplemented!()
+    }
 }
