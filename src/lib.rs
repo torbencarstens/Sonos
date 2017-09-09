b06dd137d48647855b105d5351e24e6bc37f6ebe
chabare <chabare95@gmail.com>
2017-09-09 20:05:48 +0200

    Use log in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate log;
 extern crate sonos_discovery;
 
 mod device;
@@ -10,8 +12,15 @@ use std::{collections, net};
 
 
 pub fn discover(timeout: Option<u32>, device_count: Option<usize>) -> Result<collections::HashSet<net::IpAddr>, SonosError> {
+    debug!("Start discovery");
     match Discover::new()?.start(timeout, device_count) {
-        Ok(set) => Ok(set),
-        Err(e) => Err(SonosError::from(e))
+        Ok(set) => {
+            debug!("Successfully discovered {} devices: [{}]", set.len(), set.iter().map(|ip| format!("{}", ip)).collect::<Vec<String>>().join(", "));
+            Ok(set)
+        }
+        Err(e) => {
+            debug!("Failed to discover devices: {:#?}", e);
+            Err(SonosError::from(e))
+        }
     }
 }
